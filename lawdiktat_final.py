9acabbe318c63553ad3f1f464725a948f9e92adc
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2022-05-12 18:13:40 +0530

    Update lawdiktat_final.py

--- lawdiktat_final.py
+++ lawdiktat_final.py
@@ -33,7 +33,10 @@ def displayPDF(file):
     #Opening file from file path
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    components.iframe(src=file ,width=900, height=1000,scrolling=True)
+    components.html(
+        <iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="1000" type="application/pdf"></iframe>
+    )
+#     components.iframe(src=file ,width=900, height=1000,scrolling=True)
 
 #     # Embedding PDF in HTML
 #     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="1000" type="application/pdf"></iframe>'
