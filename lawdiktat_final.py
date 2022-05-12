2674f961eebb8d76a9fb6295a8c3d9673a2c5458
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2022-05-12 17:44:23 +0530

    Update lawdiktat_final.py

--- lawdiktat_final.py
+++ lawdiktat_final.py
@@ -33,14 +33,14 @@ def displayPDF(file):
     #Opening file from file path
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    components.iframe(src=base64_pdf ,width=900, height=1000,scrolling=True)
+#     components.iframe(src=base64_pdf ,width=900, height=1000,scrolling=True)
 
     # Embedding PDF in HTML
-#     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="1000" type="application/pdf"></iframe>'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="1000" type="application/pdf"></iframe>'
     
     
    # Displaying File
-#     st.markdown(pdf_display, unsafe_allow_html=True)
+    st.markdown(pdf_display, unsafe_allow_html=True)
 #     st.markdown(pdf_display)
 
 def triggerfunction(file, file_name):
